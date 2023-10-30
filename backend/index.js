c35be1728a9214ae93053ed80b66c79d57db0e6f
pyogi37 <pyogi37@gmail.com>
2023-10-30 16:17:54 +0530

    resolved deployment bugs

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use("/", require("./routes"));.
+app.use("/", require("./routes"));
 
 //---------------------------DEPLOYMENT CODE----------------------------------
 
