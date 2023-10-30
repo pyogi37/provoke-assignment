f2be20b5caa2ce8cd3fdc830627e4594b56dfe1b
pyogi37 <pyogi37@gmail.com>
2023-10-30 16:32:28 +0530

    resolved cors bugs

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -45,7 +45,7 @@ const Login = () => {
       };
 
       const data = await axios.post(
-        "http://127.0.0.1:8000/api/v1/user/login",
+        "/api/v1/user/login",
         { email, password },
         config
       );
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -58,7 +58,7 @@ const Signup = () => {
       };
 
       const { data } = await axios.post(
-        "http://127.0.0.1:8000/api/v1/user",
+        "/api/v1/user",
         { name, email, password },
         config
       );
